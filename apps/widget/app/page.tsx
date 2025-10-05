54c6d946e86903f7fd5110eaa2efc18bdc7429db
Yousuf <124455652+yousufSadeqi@users.noreply.github.com>
2025-10-05 11:09:10 +0200

    Frist commit

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,15 +1,11 @@
 import { Button } from "@workspace/ui/components/button"
-import {add} from "@workspace/math/add"
-import {Input} from "@workspace/ui/components/input"
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
+        <h1 className="text-2xl font-bold">Hello COming</h1>
         <Button size="sm">Button</Button>
-        <p>{add(2 , 4)}</p>
-        <Input placeholder="Input" />
       </div>
     </div>
   )
